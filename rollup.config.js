0427a5945b96fb97738ed877cea84a807dcca483
Filipi Silva <filipi.silva@hitechnologies.com.br>
2023-03-24 22:02:11 -0300

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+
+const config = {
+  input: "src/lib/index.ts",
+  output: [
+    {
+      file: "build/index.js",
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: "build/index.es.js",
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve({
+      preferBuiltins: true,
+      mainFields: ['browser']
+    }),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss()
+  ],
+  external: ['react', 'react-dom']
+};
+
+export default config
