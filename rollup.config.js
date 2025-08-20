151af07cae5af44ccb548299e8be939fb0f5e446
filipigustavo-gb <107497449+filipigustavo-gb@users.noreply.github.com>
2025-08-20 14:34:02 -0300

    fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,44 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import typescript from '@rollup/plugin-typescript';
 import postcss from "rollup-plugin-postcss";
 import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { readFileSync } from 'fs';
 
-const config = {
-  input: "src/lib/index.ts",
-  output: [
-    {
-      file: "build/index.js",
-      format: "cjs",
-      sourcemap: true
-    },
-    {
-      file: "build/index.es.js",
-      format: "esm",
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    resolve({
-      preferBuiltins: true,
-      mainFields: ['browser']
-    }),
-    commonjs(),
-    terser(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    postcss()
-  ],
-  external: ['react', 'react-dom']
-};
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
+
+const config = [
+  {
+    input: "src/lib/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve({
+        preferBuiltins: true,
+        mainFields: ['browser'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }),
+      commonjs(),
+      typescript({
+        rootDir: "src/lib"
+      }),
+      postcss(),
+      terser()
+    ],
+    external: ['react', 'react-dom']
+  }
+];
 
 export default config
